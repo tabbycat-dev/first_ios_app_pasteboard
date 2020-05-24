b807dc1395fa67a2c4e2cf16964f59cc06f46002
Tan Nguyen Tabby <tannguyen.tabby@gmail.com>
2020-05-25 02:01:15 +1000

    init commit

--- Plain Ol' PasteBoard/SceneDelegate.swift
+++ Plain Ol' PasteBoard/SceneDelegate.swift
@@ -30,6 +30,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        if let vc = window?.rootViewController as? ViewController{
+            vc.addText()
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
--- Plain Ol' PasteBoard/ViewController.swift
+++ Plain Ol' PasteBoard/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let DATA_KEY = "data_key"
+    @IBOutlet weak var textViewTop: UITextView!
+    var pastedStrings : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let loadedStrings = UserDefaults.standard.stringArray(forKey: DATA_KEY){
+            pastedStrings.append(contentsOf: loadedStrings) //retrieve all text from persistent storage and save to array
+        }
         // Do any additional setup after loading the view.
+        showText()
     }
-
-
+    func addText(){
+        guard let text = UIPasteboard.general.string, !pastedStrings.contains(text) else{
+            return // only new text, dont save twice
+        }
+        
+        pastedStrings.append(text) //added new text to array
+        UserDefaults.standard.set(pastedStrings, forKey: DATA_KEY) // save text to persistent storage
+        showText()
+    }
+    
+    func showText(){
+        textViewTop.text = ""
+        for str in pastedStrings {
+            textViewTop.text.append("\(str)\n---\n") //show all text in an array
+        }
+    }
+    
+    @IBAction func button(_ sender: UIBarButtonItem) {
+        pastedStrings.removeAll()
+        textViewTop.text =  ""
+        UserDefaults.standard.removeObject(forKey: DATA_KEY)
+    }
 }
 
